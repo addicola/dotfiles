eab692f6c04296a10d2ebabe5ef562e61a34bde2
Nuttapon Pramoolsin <blue_lifesky@hotmail.com>
2024-03-24 14:21:47 +0700

    added nvim config

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = function()
+      return {
+        style = "storm",
+        transparent = false,
+      }
+    end,
+  },
+}
---
+++ config/nvim/lua/plugins/tools.lua
@@ -0,0 +1,12 @@
+return {
+	-- Better diffing
+	{
+		"sindrets/diffview.nvim",
+    -- stylua: ignore start
+    cmd = {
+      "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles",
+      "DiffviewFileHistory", "DiffviewRefresh"
+    },
+		-- stylua: ignore end
+	},
+}
